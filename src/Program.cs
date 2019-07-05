43dc19fcaefd717c9a749409253d0ce90b1fe7da
jetiba-ms <jessica.tibaldi@microsoft.com>
2019-07-05 14:41:44 +0200

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+
+namespace msgprocessor
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            Console.WriteLine(Environment.GetEnvironmentVariable("EventHubName") + " " + 
+                        PartitionReceiver.DefaultConsumerGroupName + " " +
+                        Environment.GetEnvironmentVariable("EventHubConnectionString") + " " +
+                        Environment.GetEnvironmentVariable("StorageConnectionString") + " " +
+                        Environment.GetEnvironmentVariable("StorageContainerName"));
+
+            var eventProcessorHost = new EventProcessorHost(
+                        Environment.GetEnvironmentVariable("EventHubName"),
+                        PartitionReceiver.DefaultConsumerGroupName,
+                        Environment.GetEnvironmentVariable("EventHubConnectionString"),
+                        Environment.GetEnvironmentVariable("StorageConnectionString"),
+                        Environment.GetEnvironmentVariable("StorageContainerName"));
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            while (true)
+            {
+                await Task.Delay(30000);
+            }
+        }
+    }
+}
